89ba1ec7111a3f8b53c8ca733cb80eaaaae22f75
unknown <ken.brandon@aol.com>
2020-02-27 23:05:39 -0700

    styled but button dont go anywhere

--- secret-family-recipes-cookbook-4/src/components/RegistrationForm.js
+++ secret-family-recipes-cookbook-4/src/components/RegistrationForm.js
@@ -1,167 +1,110 @@
-import React, { useState, useEffect } from "react";
-import { Form, Field, withFormik } from "formik";
-import * as yup from "yup";
-import axios from "axios";
-import styled from "styled-components";
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
 
-const Button = styled.button`
-  font-size: 22px;
-  padding: 5px 20px;
-  border-radius: 2px;
-  margin: 10px 0 30px 0;
-  background: goldenrod;
+function RegistrationForm() {
+  const [newUser, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    confirmPassword: ""
+  });
 
-  &:hover {
-    background: #fff;
-    cursor: pointer;
-  }
-`;
+  const handleChange = event => {
+    setUser({ ...newUser, [event.target.name]: event.target.value });
+  };
 
-function InputForm({ errors, touched, isSubmitting, status }) {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    status && setUsers([...users, status]);
-  }, [status, users]);
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
 
   return (
-    <div className="form-wrapper">
-      <Form>
-        <label>
-          <Field
-            type="text"
-            name="firstName"
-            placeholder="First Name"
-            style={{ border: "1.5px solid green" }}
-          />
-          {touched.firstName && errors.firstName && (
-            <h3 className="error" style={{ color: "green" }}>
-              {errors.firstName}
-            </h3>
-          )}
-        </label>
-
-        <label>
-          <Field
-            type="text"
-            name="lastName"
-            placeholder="Last Name"
-            style={{ border: "1.5px solid blue" }}
-          />
-          {touched.lastName && errors.lastName && (
-            <h3 className="error" style={{ color: "blue" }}>
-              {errors.lastName}
-            </h3>
-          )}
-        </label>
-
-        <label>
-          <Field
-            type="email"
-            name="email"
-            placeholder="Email"
-            style={{ border: "1.5px solid orange" }}
-          />
-          {touched.email && errors.email && (
-            <h3 className="error" style={{ color: "orange" }}>
-              {errors.email}
-            </h3>
-          )}
-        </label>
-
-        <label>
-          <Field
-            type="password"
-            name="password"
-            placeholder="Password"
-            style={{ border: "1.5px solid #82320a" }}
-          />
-          {touched.password && errors.password && (
-            <h3 className="error" style={{ color: "#82320a" }}>
-              {errors.password}
-            </h3>
-          )}
-        </label>
-
-        <label>
-          <Field
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            style={{ border: "1.5px solid orange" }}
-          />
-          {touched.confirmPassword && errors.confirmPassword && (
-            <h3 className="error" style={{ color: "orange" }}>
-              {errors.confirmPassword}
-            </h3>
-          )}
-        </label>
-
-        <label>
-          Accept Terms of Service
-          <Field type="checkbox" name="termsOfService" />
-          <span className="checkmark" />
-        </label>
-
-        <div>
-          <Button type="submit" disabled={isSubmitting}>
-            {isSubmitting ? "Submitting" : "Submit"}
-          </Button>
-        </div>
+    <Container>
+      <Form onSubmit={event => handleSubmit(event)}>
+        <Col>
+          <FormGroup>
+            <Label htmlFor="firstname">Firstname: </Label>
+            <Input
+              id="firstname"
+              type="text"
+              name="firstname"
+              placeholder="Enter Your First Name"
+              required
+              value={newUser.firstname}
+              onChange={event => handleChange(event)}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label htmlFor="lastname">Lastname: </Label>
+            <Input
+              id="lastname"
+              type="text"
+              name="lastname"
+              placeholder="Enter Your Last Name"
+              required
+              value={newUser.lastname}
+              onChange={event => handleChange(event)}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label htmlFor="username">Username: </Label>
+            <Input
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Enter Your Username"
+              required
+              value={newUser.username}
+              onChange={event => handleChange(event)}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label htmlFor="password">Password: </Label>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Enter Your Password"
+              required
+              value={newUser.password}
+              onChange={event => handleChange(event)}
+            />
+          </FormGroup>
+        </Col>
+        {/* <Col>
+          <FormGroup>
+            <Label htmlFor="confirmPassword">Confirm Password:</Label>
+            <Input
+              id="confirmPassword"
+              type="password"
+              name="password"
+              placeholder="Confirm Your Password"
+              required
+              value={newUser.confirmPassword}
+              onChange={event => handleChange(event)}
+            />
+          </FormGroup>
+        </Col> */}
+        <Button type="submit" id="submit-btn">
+          Submit!
+        </Button>
       </Form>
-    </div>
+    </Container>
   );
 }
 
-export default withFormik({
-  mapPropsToValues: () => ({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    termsOfService: false
-  }),
-
-  validationSchema: yup.object().shape({
-    firstName: yup
-      .string()
-      .required("Please Enter Your First Name.")
-      .matches(/^[a-zA-Z]+$/, "Must contain alphabet letters only."),
-
-    lastName: yup
-      .string()
-      .required("Please Enter Your Last Name.")
-      .matches(/^[a-zA-Z]+$/, "Must contain alphabet letters only."),
-    email: yup
-      .string()
-      .required("Please Enter Your Email.")
-      .email("Sorry, that's not a valid email."),
-    password: yup
-      .string()
-      .required("Please Enter Your Password.")
-      .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-        "Must Contain 8 Characters, 1 Uppercase, 1 Lowercase, 1 Number and 1 special case Character."
-      ),
-    confirmPassword: yup
-      .string()
-      .required("Please Confirm Your Password.")
-      .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-        "Must Contain 8 Characters, 1 Uppercase, 1 Lowercase, 1 Number and 1 special case Character."
-      )
-  }),
-
-  handleSubmit: (values, formikBag) => {
-    const { resetForm, setStatus } = formikBag;
-
-    axios
-      .post("https://reqres.in/api/users", values)
-      .then(response => {
-        setStatus(response.data);
-        resetForm();
-      })
-      .catch(error => console.error(error));
-  }
-})(InputForm);
+export default RegistrationForm;
