dd2f5004ec2069c1d4e5c62ed24e1a0039bb0ba5
unknown <ken.brandon@aol.com>
2020-03-02 20:28:46 -0700

    added a little styling to links

--- secret-family-recipes-cookbook-4/src/App.js
+++ secret-family-recipes-cookbook-4/src/App.js
@@ -3,24 +3,25 @@ import { Route, Link, Switch } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import RegistrationForm from "./components/RegistrationForm";
 import RecipeForm from "./components/RegistrationForm";
-import Login from "./components/Login";
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
+import { Navbar, NavbarText } from "reactstrap";
 
 function App() {
   return (
     <main>
-      <div>
+      <Navbar className="navbar">
         <Link to="/login">Login</Link>
         <Link to="/registrationForm">Sign Up</Link>
         <Link to="/recipes">Recipes</Link>
-      </div>
+        <NavbarText>Photo by Skitterphoto from Pexels</NavbarText>
+      </Navbar>
       <Switch>
         <PrivateRoute exact path="/recipes" component={RecipeForm} />
         <Route path="/registrationForm" component={RegistrationForm} />
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={LoginForm} />
       </Switch>
-      <footer>Photo by Narda Yescas from Pexels</footer>
     </main>
   );
 }
